9bb64d798110c403f9c7af288708b3cfe24f0ca6
RoniaZhang <XiaQian1999@gmail.com>
2024-08-02 22:53:29 -0400

    Initial Set up for the header banner section

---
+++ src/components/ExplorePost/ExplorePost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExplorePost = () => {
+  return (
+    <div>ExplorePost</div>
+  )
+}
+
+export default ExplorePost
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import ExplorePost from '../../components/ExplorePost/ExplorePost'
+// import './Home.css';
+
+const Home = () => {
+
+  return (
+    <div>
+      <Header />
+      <ExplorePost />
+    </div>
+  )
+}
+
+export default Home
