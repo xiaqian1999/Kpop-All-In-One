426ab86323e0904710164bc9572843e7bef70067
RoniaZhang <XiaQian1999@gmail.com>
2024-08-03 17:21:56 -0400

    Initialize with StoreContext for prop drilling

---
+++ src/context/StoreContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from 'react'
+import { posts_list } from '../assets/assets'
+
+
+// createContext is a function that creates a Context object. This Contect object allows you to share data between components without having to pass props down through every level of the component TreeWalker, a procss known as prop drilling
+export const StoreContext = createContext(null)
+
+const StoreContextProvider = (props) => {
+
+    const [bookmarkItems, setBookmarkItems] = useState({});
+
+    const addToBookmark = (itemId) => {
+        //When user click on the empty star icon, they are able to bookmark the post and initialize a count with 1
+        setBookmarkItems((prev) => ({...prev, [itemId]:1}))
+    }
+
+    const removeFromBookmark = (itemId) => {
+        //When user click on the full star icon, they are able to remove the post from bookmark record
+        setBookmarkItems((prev) => ({...prev, [itemId]:0}))
+    }
+
+    const contextValue = {
+        posts_list, bookmarkItems, setBookmarkItems, addToBookmark, removeFromBookmark
+    }
+
+    return (
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+export default StoreContextProvider
