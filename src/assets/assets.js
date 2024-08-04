1ca5b0ba259b80378ddbbe1844a9c8b6aedd5ecd
RoniaZhang <XiaQian1999@gmail.com>
2024-08-03 20:57:50 -0400

    Scammer assets list added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -106,7 +106,7 @@ export const posts_list = [
         insta_url: "https://www.instagram.com/p/C3RmsqnuIa3/?igsh=aHlxeDFlNW0yZDZw",
         post_image: insta_post_img_08,
         transaction_type: "WTS",
-        kpop_group: ["Seventeen", "Twice", "Ive"],
+        kpop_group: ["Seventeen", "Ive"],
         description: "Example of note, have: XYZ, want to sell for $3 each",
     },
     {
@@ -116,5 +116,151 @@ export const posts_list = [
         transaction_type: "WTS",
         kpop_group: ["Twice"],
         description: "Example of note, have: XYZ, want to sell for $3 each",
+    }
+]
+
+export const scammer_post_list = [
+    {
+        _id: "1",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "hihighsales",
+        date_of_post: "11/2/2018",
+        user_type: "seller",
+        platform_transaction: "instagram",
+        scammer_type: "Potential Scammer",
+        kpop_group: ["Seventeen", "BTS"],
+        payment_info :"test123@gmail.com"
+    },
+    {
+        _id: "2",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "teampoger",
+        date_of_post: "11/2/2018",
+        user_type: "gom",
+        platform_transaction: "instagram",
+        scammer_type: "Potential Scammer",
+        kpop_group: ["NCT", "BTS"],
+        payment_info :"test123@gmail.com"
+    },
+    {
+        _id: "3",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "teampoger",
+        date_of_post: "11/2/2018",
+        user_type: "seller",
+        platform_transaction: "instagram",
+        scammer_type: "Bad Experience",
+        kpop_group: ["NCT", "BTS"],
+        payment_info :"test123@gmail.com"
+    },
+    {
+        _id: "4",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "hihighsales",
+        date_of_post: "11/2/2018",
+        user_type: "seller",
+        platform_transaction: "instagram",
+        scammer_type: "Bad Experience",
+        kpop_group: ["Seventeen", "BTS"],
+        payment_info :"test123@gmail.com"
+    },
+    {
+        _id: "5",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "hihighsales",
+        date_of_post: "11/2/2018",
+        user_type: "trader",
+        platform_transaction: "instagram",
+        scammer_type: "Potential Scammer",
+        kpop_group: ["Seventeen", "Twice"],
+        payment_info :"test123@gmail.com"
+    },
+    {
+        _id: "6",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "bxby_sssnoopy",
+        date_of_post: "11/2/2018",
+        user_type: "trader",
+        platform_transaction: "twitter",
+        scammer_type: "Potential Scammer",
+        kpop_group: ["Seventeen", "Twice"],
+        payment_info :"abc123@gmail.com"
+    },
+    {
+        _id: "7",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "bxby_snoopy",
+        date_of_post: "11/2/2018",
+        user_type: "trader",
+        platform_transaction: "twitter",
+        scammer_type: "Potential Scammer",
+        kpop_group: ["Seventeen", "Twice"],
+        payment_info :"abc1234@gmail.com"
+    },
+    {
+        _id: "8",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "bxby_snoopy",
+        date_of_post: "11/2/2018",
+        user_type: "seller",
+        platform_transaction: "twitter",
+        scammer_type: "Potential Scammer",
+        kpop_group: ["EXO", "Twice"],
+        payment_info :"abc123@gmail.com"
+    },
+    {
+        _id: "9",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "hihighsales",
+        date_of_post: "11/2/2018",
+        user_type: "seller",
+        platform_transaction: "instagram",
+        scammer_type: "Potential Scammer",
+        kpop_group: ["EXO", "Twice"],
+        payment_info :"test123@gmail.com"
+    },
+    {
+        _id: "10",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "hihighsales",
+        date_of_post: "11/2/2018",
+        user_type: "seller",
+        platform_transaction: "instagram",
+        scammer_type: "Potential Scammer",
+        kpop_group: ["Seventeen", "IVE"],
+        payment_info :"ttest123@gmail.com"
+    },
+    {
+        _id: "11",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "ggukop",
+        date_of_post: "11/2/2018",
+        user_type: "seller",
+        platform_transaction: "ebay",
+        scammer_type: "Potential Scammer",
+        kpop_group: ["Straykids", "Ateez"],
+        payment_info :"ttest123@gmail.com"
+    },
+    {
+        _id: "12",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "ggukop",
+        date_of_post: "11/2/2018",
+        user_type: "buyer",
+        platform_transaction: "instagram",
+        scammer_type: "Selective Scammer",
+        kpop_group: ["Straykids", "Ateez"],
+        payment_info :"test123@gmail.com"
+    },
+    {
+        _id: "13",
+        insta_url: "https://www.instagram.com/p/Bps7v0TgUiK/?utm_medium=copy_link",
+        username: "hihighsalesss",
+        date_of_post: "11/2/2018",
+        user_type: "seller",
+        platform_transaction: "depop",
+        scammer_type: "Selective Scammer",
+        kpop_group: ["TXT"],
+        payment_info :"test123@gmail.com"
     }
 ]
