07ec1063e4899b6f79c23b7b87e6f631f8f73a0f
RoniaZhang <XiaQian1999@gmail.com>
2024-08-06 22:19:49 -0400

    Update Navbar with Link from react router dom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 // import './Navbar.css';
 import { assets } from '../assets/assets';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = ({setShowLogin}) => {
@@ -8,17 +9,18 @@ const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("home");
   return (
     <div className='flex justify-between items-center py-10'>
-      <img src={assets.logo} alt="logo" className='w-[150px]' />
+      <Link to="/"><img src={assets.logo} alt="logo" className='w-[150px]' /></Link>
+
       <ul className='flex list-none gap-[20px] text-lg items-center'>
-        <li onClick={() => {setMenu("home")}} className={`hover:text-red-300 cursor-pointer ${menu==="home" ? "border-b-red-300 text-red-300 " : ""}`}><a href="#home">Home</a></li>
-        <li onClick={() => {setMenu("transaction-section")}} className={`hover:text-red-300 cursor-pointer ${menu==="transaction-section" ? "border-b-red-300 text-red-300 " : ""}`}><a href="#transaction-section">Transaction</a></li>
-        <li onClick={() => {setMenu("scammer-section")}} className={`hover:text-red-300 cursor-pointer ${menu==="scammer-section" ? "border-b-red-300 text-red-300 " : ""}`}><a href="#scammer-section">Scammer Search</a></li>
-        <li onClick={() => {setMenu("contact-us")}} className={`hover:text-red-300 cursor-pointer ${menu==="contact-us" ? "border-b-red-300 text-red-300 " : ""}`}><a href="#contact-us">Contact Us</a></li>
+        <Link to="/" onClick={() => {setMenu("home")}} className={`hover:text-red-300 cursor-pointer ${menu==="home" ? "border-b-red-300 text-red-300 " : ""}`}>Home</Link>
+        <a href="#transaction-section" onClick={() => {setMenu("transaction-section")}} className={`hover:text-red-300 cursor-pointer ${menu==="transaction-section" ? "border-b-red-300 text-red-300 " : ""}`}>Transaction</a>
+        <a href="#scammer-section" onClick={() => {setMenu("scammer-section")}} className={`hover:text-red-300 cursor-pointer ${menu==="scammer-section" ? "border-b-red-300 text-red-300 " : ""}`}>Scammer Search</a>
+        <a href="#contact-us" onClick={() => {setMenu("contact-us")}} className={`hover:text-red-300 cursor-pointer ${menu==="contact-us" ? "border-b-red-300 text-red-300 " : ""}`}>Contact Us</a>
       </ul>
 
       <div className='flex items-center gap-[40px]'>
         <div>
-          <img src={assets.bookmark_before} alt="bookmark icon" className='relative w-5 h-5 cursor-pointer' />
+          <Link to="/bookmark"><img src={assets.bookmark_before} alt="bookmark icon" className='relative w-5 h-5 cursor-pointer' /></Link>
           <div className='absolute min-w-2 min-h-2 bg-red-500 rounded-full top-[-8px] right-[-8px]'>2</div>
         </div>
 
