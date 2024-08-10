867badea7eccdb367caa749933beccf886f31396
RoniaZhang <XiaQian1999@gmail.com>
2024-08-09 21:05:54 -0400

    Admin: Create onchangeHandler function for the form

--- admin/src/pages/AddPost.jsx
+++ admin/src/pages/AddPost.jsx
@@ -1,8 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 
 const AddPost = () => {
     const [image, setImage] = useState(false)
+    const [data, setData] = useState({
+        insta_url: "",
+        transaction_type: "",
+        // transaction_type: "WTS", this is if you want to have as default
+        kgroup_name: "",
+        description: ""
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data => ({...data, [name]:value}))
+    }
+
+    useEffect(() => {
+        console.log(data)
+    }, [data])
+
   return (
     <div className='w-[70%] ml-5 mt-5 text-gray-600'>
         <form className='flex flex-col gap-4 items-center'>
@@ -17,12 +35,12 @@ const AddPost = () => {
 
             <div className="flex flex-col w-[280px]">
                 <p>Instagram URL</p>
-                <input type="url" name='insta_url' placeholder='Type here' className='p-3 border border-gray-400 rounded-md' />
+                <input onChange={onChangeHandler} value={data.insta_url} type="url" name='insta_url' placeholder='Type here' className='p-3 border border-gray-400 rounded-md' />
             </div>
 
             <div className="flex flex-col w-[280px]">
                 <p>Transaction Type</p>
-                <select name="transaction_type" className='p-3 border border-gray-400 rounded-md' >
+                <select onChange={onChangeHandler} name="transaction_type" className='p-3 border border-gray-400 rounded-md' >
                     <option value="WTS">WTS</option>
                     <option value="WTT">WTT</option>
                     <option value="WTB">WTB</option>
@@ -32,7 +50,7 @@ const AddPost = () => {
 
             <div className="flex flex-col w-[280px]" >
                 <p>Kpop Group</p>
-                <select name="transaction_type" className='p-3 border border-gray-400 rounded-md'>
+                <select onChange={onChangeHandler} name="kgroup_name" className='p-3 border border-gray-400 rounded-md'>
                     <option value="Seventeen">Seventeen</option>
                     <option value="Astro">Astro</option>
                     <option value="Twice">Twice</option>
@@ -42,8 +60,8 @@ const AddPost = () => {
             </div>
 
             <div className="flex flex-col w-[280px]">
-                <p>Fescription</p>
-                <textarea type="text" name='insta_url' placeholder='Write content here' className='p-3 border border-gray-400 rounded-md'  required></textarea>
+                <p>Description</p>
+                <textarea onChange={onChangeHandler} value={data.description} type="text" name='description' placeholder='Write content here' className='p-3 border border-gray-400 rounded-md'  required></textarea>
             </div>
 
             <button type="submit" className='bg-red-300 hover:bg-red-400 cursor-pointer px-4 py-2 rounded-md w-[280px]'>Add</button>
